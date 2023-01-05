ad489cc14962bb3ea8d2089a37af7c6ab0e05018
JakeMcG9 <jakemcgrory9@gmail.com>
2023-01-04 20:46:43 -0500

    Add focus button to trigger DOM cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -29,11 +29,17 @@ disableButton.id = '_rf_disablebtn';
 disableButton.classList.add('_rfbtn');
 disableButton.textContent = 'disable on this site';
 
+const focusButton = document.createElement('button');
+focusButton.id = '_rf_focusbtn';
+focusButton.classList.add('_rfbtn');
+focusButton.textContent = 'focus on recipe';
+
 const controls = document.createElement('div');
 controls.id  = '_rf_header';
 controls.appendChild(closeButton);
 controls.appendChild(document.createTextNode('Recipe Filter'));
 controls.appendChild(disableButton);
+controls.appendChild(focusButton);
 
 function hidePopup(){
 	let highlight = document.getElementById('_rf_highlight');
@@ -52,38 +58,67 @@ function reshowPopup() {
 	existingPopup.style.opacity = 1;
 }
 
+let clone = null;
+let focus = false;
+function toggleFocus() {
+	if (!focus) {
+		let recipe = document.getElementById('_rf_recipe');
+
+		// remove problematic elements
+		remove_selectors.forEach(function(s) {
+			recipe.querySelectorAll(s).forEach(function(n) {
+				n.remove();
+			})
+		})
+
+		focus = true;
+		focusButton.textContent = 'show complete recipe';
+	} else {
+		let recipe = document.getElementById('_rf_recipe');
+		recipe.remove();
+
+		let popup = document.getElementById('_rf_highlight');
+		recipe = clone.cloneNode(true);
+		recipe.id = '_rf_recipe';
+		popup.appendChild(recipe);
+
+		focus = false;
+		focusButton.textContent = 'focus on recipe';
+	}
+}
+
 function showPopup(){
 	recipe_selectors.every(function(s){
 		let original = document.querySelector(s);
 		if (original){
 			// clone the matched element
-			let clone = original.cloneNode(true);
+			clone = original.cloneNode(true);
+			recipe = clone.cloneNode(true);
+			recipe.id = '_rf_recipe';
 
-			// remove problematic elements
-			remove_selectors.forEach(function(s) {
-				clone.querySelectorAll(s).forEach(function(n) {
-					n.remove();
-				})
-			})
+			popup = document.createElement('div');
+			popup.id = '_rf_highlight';
 
-			clone.id = '_rf_highlight';
+			popup.appendChild(controls);
+			popup.appendChild(recipe);
 			// add some control buttons
-			clone.prepend(controls);
-			clone.style.transition = 'opacity 500ms';
-			clone.style.display = 'block';
-			clone.style.opacity = 0;
+			popup.style.transition = 'opacity 500ms';
+			popup.style.display = 'block';
+			popup.style.opacity = 0;
 
-			document.body.insertBefore(clone, document.body.firstChild);
+			document.body.insertBefore(popup, document.body.firstChild);
 
 			// handle the two new buttons we attached to the popup
 			closeButton.addEventListener('click', hidePopup);
 			disableButton.addEventListener('click', function(b){
 				chrome.storage.sync.set({[document.location.hostname]: true}, hidePopup);
 			});
+			focusButton.addEventListener('click', toggleFocus);
 
 			// add an event listener for clicking outside the recipe to close it
 			let mouseUpHide = function(e) {
-				if (e.target !== clone && !clone.contains(e.target) && event.target.type !== 'submit')
+				console.log(e.target);
+				if (e.target !== popup && !popup.contains(e.target) && event.target.type !== 'submit')
 				{
 						hidePopup();
 						document.removeEventListener('mouseup', mouseUpHide);
@@ -93,7 +128,7 @@ function showPopup(){
 
 			window.setTimeout(() => {
 				// fade in
-				clone.style.opacity = 1;
+				popup.style.opacity = 1;
 
 				// scroll to top in case they hit refresh while lower in page
 				document.scrollingElement.scrollTop = 0;
